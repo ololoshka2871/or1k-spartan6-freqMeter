0161746280f7d1513d9b8f957eb368f4cc4b1e3f
ololoshka2871 <SweetTreasure@2ch.hk>
2016-11-30 16:03:12 +0300

    adding tests for protobuf protocol

---
+++ tests/pb_tester.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    print('Runned')
+    pass
+
+# чтобы при импорте не выполнялся код автоматом
+if __name__ == '__main__':
+    main()
